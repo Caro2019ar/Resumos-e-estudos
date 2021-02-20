f03b5e4a78a4f2973335939c049a6919904dcfcb
Carolina <argente2019@gmail.com>
2021-02-19 23:47:57 -0300

    Clase12-Modularizar

--- CoderHouse/Clase12-Websocket1/app.ts
+++ CoderHouse/Clase12-Websocket1/app.ts
@@ -5,11 +5,11 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const bodyParser = require("body-parser");
 const port = 8080;
-const productos = require("./rutas/productos");
+//const productos = require("./rutas/productos");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api/productos", productos);
+// app.use("/api/productos", productos);
 
 const handlebars = require("express-handlebars");
 
@@ -26,13 +26,39 @@ app.engine(
 app.set("views", "./views");
 app.set("view engine", "hbs");
 
+app.get("/", (req: Request, res: Response) => {
+	res.render("ingrese");
+});
+let messages: any = [
+	{ email: "Juan@gmail.com", text: "¡Hola! ¿Que tal?" },
+	{ email: "Pedro@yahoo.com", text: "¡Muy bien! ¿Y vos?" },
+	{ email: "Ana@hotmail.com", text: "¡Genial!" },
+];
+
+let produtos: any = [];
+
 io.on("connection", (socket: any) => {
-	console.log("Usuario conectado");
-	socket.emit("mi mensaje", "Este es mi mensaje desde el servidor");
-	socket.on("notificacion", (data: any) => {
-		console.log("notificacion", data);
+	console.log("Nuevo cliente conectado");
+	socket.emit("messages", messages);
+	socket.on("new-message", function (data: any) {
+		messages.push(data);
+		io.sockets.emit("messages", messages);
+	});
+	socket.emit("produtos", produtos);
+	socket.on("new-produto", function (produto: any) {
+		produtos.push(produto);
+		io.sockets.emit("produtos", produtos);
 	});
 });
+
+//--- OK:
+// io.on("connection", (socket: any) => {
+// 	console.log("Usuario conectado");
+// 	socket.emit("mi mensaje", "Este es mi mensaje desde el servidor");
+// 	socket.on("notificacion", (data: any) => {
+// 		console.log("notificacion", data);
+// 	});
+// });
 
 http.listen(port, () => {
 	console.log(`Conexión al puerto ${port}`);
